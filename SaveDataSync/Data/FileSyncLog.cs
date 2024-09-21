ab3546b8fdcdff4d13a235ea7fed7959b7ac630c
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-09-21 19:09:52 +0100

    Added 'FileSyncLog' to store a file sync operation log

---
+++ SaveDataSync/Data/FileSyncLog.cs
@@ -0,0 +1,26 @@
+﻿namespace SaveDataSync.Data
+{
+	public class FileSyncLog
+	{
+		public string FileName { get; init; }
+
+		public DateTime LastModified { get; init; }
+		
+		public string SyncStatus { get; init; }
+		
+		public string DestinationPath { get; init; }
+
+
+		public FileSyncLog(
+			string fileName,
+			DateTime lastModified, 
+			string syncStatus, 
+			string destinationPath)
+		{
+			FileName = fileName;
+			LastModified = lastModified;
+			SyncStatus = syncStatus;
+			DestinationPath = destinationPath;
+		}
+	}
+}
