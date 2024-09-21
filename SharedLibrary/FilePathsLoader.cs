a46b8482980663512d2d401718d84d13f9860a7b
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-09-21 18:57:25 +0100

    Added a static utility function to load file paths from a file (FilePaths.json)

---
+++ SharedLibrary/FilePathsLoader.cs
@@ -0,0 +1,15 @@
+﻿using SaveDataSync.Providers.Factory;
+
+
+namespace SharedLibrary
+{
+	public class FilePathsLoader
+	{
+		public static T LoadFilePaths<T>()
+		{
+			var saveFilePaths = Path.Combine(Directory.GetCurrentDirectory(), "StaticData/FilePaths.json");
+
+			return Factory.GetProduct<JsonDeserializerFactory<T>, T, string>(saveFilePaths);
+		}
+	}
+}
