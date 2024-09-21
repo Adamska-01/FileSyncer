66ed1195eb2a880a3b3f449be914597416dd01eb
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-09-21 18:56:31 +0100

    Added a generic 'JsonDeserializerFactory' to deserialize any json file

---
+++ SharedLibrary/Factory/JsonDeserializerFactory .cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+
+namespace SaveDataSync.Providers.Factory
+{
+	using Interfaces;
+
+
+	public class JsonDeserializerFactory<TReturn> : IFactory<TReturn, string>
+	{
+		TReturn IFactory<TReturn, string>.CreateProduct(string jsonPath)
+		{
+			var paths = File.ReadAllText(jsonPath);
+			var savePaths = JsonSerializer.Deserialize<TReturn>(paths) ?? throw new Exception("An error occurred while deserializing Paths.json");
+
+			return savePaths;
+		}
+	}
+}
