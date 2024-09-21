1a5f392efa87a799164c56e09be1daf7cc995158
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-09-21 19:25:33 +0100

    Added extension method to convert 'ProcessEventType' to the actual WMI Event

---
+++ TaskRunner/Provider/Extensions/ProcessEventTypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using SystemEventSubscriber.Data;
+
+
+namespace SystemEventSubscriber.Provider.Extensions
+{
+	public static class ProcessEventTypeExtensions
+	{
+		public static string ToWMIEvent(this ProcessEventType processEvent) => processEvent switch
+		{
+			ProcessEventType.START_TRACE => "Win32_ProcessStartTrace",
+			
+			ProcessEventType.STOP_TRACE => "Win32_ProcessStopTrace",
+			
+			_ => throw new ArgumentException("The \"ProcessEventType\" is not supported.", processEvent.ToString())
+		};
+	}
+}
