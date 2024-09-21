418cf986e9bcaef68282b97035a3f4766bdd28a1
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-09-21 19:19:50 +0100

    Added an enum to define process event types (WMI events)

---
+++ TaskRunner/Data/ProcessEventT ype.cs
@@ -0,0 +1,18 @@
+﻿namespace SystemEventSubscriber.Data
+{
+	/// <summary>
+	/// Specifies the types of Windows Management Instrumentation (WMI) Event.
+	/// </summary>
+	public enum ProcessEventType
+	{
+		/// <summary>
+		/// When a process starts
+		/// </summary>
+		START_TRACE,
+
+		/// <summary>
+		/// When a process stops
+		/// </summary>
+		STOP_TRACE
+	}
+}
