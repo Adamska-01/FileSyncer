07de83890a808eb036ec3df14ae19e351f5ef3d0
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-09-21 19:07:58 +0100

    Added a service to build a table in the console

---
+++ SaveDataSync/Providers/TableBuilderService.cs
@@ -0,0 +1,58 @@
+﻿using Alba.CsConsoleFormat;
+
+
+namespace SaveDataSync.Providers
+{
+	public class TableBuilderService
+	{
+		private readonly Grid grid;
+
+		private readonly LineThickness borderThickness;
+
+
+		public TableBuilderService(LineThickness borderThickness, ConsoleColor borderColor)
+		{
+			this.borderThickness = borderThickness;
+
+			grid = new Grid
+			{
+				Stroke = borderThickness,
+				StrokeColor = borderColor
+			};
+		}
+
+
+		public TableBuilderService AddColumn(int width, int maxWidth = -1)
+		{
+			grid.Columns.Add(new Column
+			{
+				Width = GridLength.Auto,
+				MinWidth = width,
+				MaxWidth = maxWidth > 0 ? maxWidth : int.MaxValue
+			});
+
+			return this;
+		}
+
+		public void AddRow(ConsoleColor textColor, Align textlignment, params string[] values)
+		{
+			foreach (var value in values)
+			{
+				grid.Children.Add(new Cell(value)
+				{
+					Stroke = borderThickness,
+					Color = textColor,
+					Align = textlignment
+				});
+			}
+		}
+
+		public Document Build()
+		{
+			var document = new Document();
+			document.Children.Add(grid);
+
+			return document;
+		}
+	}
+}
