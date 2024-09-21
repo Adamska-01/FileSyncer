37d98a07fd5d335958e5b46d846ab81aad3ae136
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-09-21 19:10:45 +0100

    Added a 'FileLocation' to deserialize 'FilePaths.json'

---
+++ SaveDataSync/Data/FileLocation.cs
@@ -0,0 +1,9 @@
+﻿namespace SaveDataSync.Data
+{
+	public readonly struct FileLocation
+	{
+		public required string CloudPath { get; init; }
+
+		public required string PhysicalPath { get; init; }
+	}
+}
