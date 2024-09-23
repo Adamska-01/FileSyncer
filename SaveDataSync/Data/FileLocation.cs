ad72226aa55347c8f6950264357d937f6fc5e4b1
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-09-23 19:22:03 +0100

    Added comments to 'FileLocation' struct

--- SaveDataSync/Data/FileLocation.cs
+++ SaveDataSync/Data/FileLocation.cs
@@ -1,9 +1,18 @@
 ﻿namespace SaveDataSync.Data
 {
+	/// <summary>
+	/// Represents the location of a file both in the cloud and on the physical storage.
+	/// </summary>
 	public readonly struct FileLocation
 	{
+		/// <summary>
+		/// The path to the file in cloud storage.
+		/// </summary>
 		public required string CloudPath { get; init; }
 
+		/// <summary>
+		/// The physical path to the file on the local storage.
+		/// </summary>
 		public required string PhysicalPath { get; init; }
 	}
 }
