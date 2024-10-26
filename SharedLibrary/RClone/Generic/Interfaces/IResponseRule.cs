188d61643858b2a8c8bbc77ff3feedb8524da208
Salvatore Galeotta <78374972+Adamska-01@users.noreply.github.com>
2024-10-26 19:50:43 +0100

    Added 'ResponseRule' class to generate user prompts based on rclone outputs

---
+++ SharedLibrary/RClone/Generic/Interfaces/IResponseRule.cs
@@ -0,0 +1,29 @@
+﻿namespace SharedLibrary.RClone.Generic.Interfaces
+{
+	/// <summary>
+	/// Defines a contract for response rules that can match an input and generate a response.
+	/// </summary>
+	public interface IResponseRule
+	{
+		/// <summary>
+		/// Gets the message associated with the response rule.
+		/// </summary>
+		string Message { get; }
+
+
+		/// <summary>
+		/// Determines whether the specified input matches the rule's pattern.
+		/// </summary>
+		/// <param name="input">The input string to check against the pattern.</param>
+		/// <returns>
+		/// <c>true</c> if the input matches the pattern; otherwise, <c>false</c>.
+		/// </returns>
+		bool IsMatch(string input);
+
+		/// <summary>
+		/// Generates a response based on the rule's response generator.
+		/// </summary>
+		/// <returns>A response string.</returns>
+		string GetResponse();
+	}
+}
---
+++ SharedLibrary/RClone/Generic/ResponseRules.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+
+namespace SharedLibrary.RClone.Generic
+{
+	using Interfaces;
+
+
+	/// <summary>
+	/// Represents a rule that matches input against a pattern and provides a corresponding response.
+	/// </summary>
+	public class ResponseRule : IResponseRule
+	{
+		private readonly Regex regex;
+		
+		private readonly Func<string> responseGenerator;
+
+
+		public string Message { get; }
+
+
+		public ResponseRule(string pattern, string message, string response)
+			: this(pattern, message, () => response) 
+		{ 
+		}
+
+		public ResponseRule(string pattern, string message, Func<string> responseGenerator)
+		{
+			regex = new Regex(pattern, RegexOptions.Compiled);
+			Message = message;
+			this.responseGenerator = responseGenerator;
+		}
+
+
+		bool IResponseRule.IsMatch(string input) 
+			=> regex.IsMatch(input);
+		
+		string IResponseRule.GetResponse() 
+			=> responseGenerator();
+	}
+}
