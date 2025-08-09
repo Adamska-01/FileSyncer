385c1a3711e6b5dc4a1c51f97d6e7dce29564804
Adamska-01 <silverworld@hotmail.it>
2025-08-09 02:31:27 +0200

    Replaced spaces with tabs in 'ProcessDetails'

--- SystemEventSubscriber/Data/ProcessDetails.cs
+++ SystemEventSubscriber/Data/ProcessDetails.cs
@@ -1,13 +1,13 @@
 ﻿namespace SystemEventSubscriber.Data
 {
-    /// <summary>
-    /// Represents details of a process, including the name of the process.
-    /// </summary>
-    public readonly struct ProcessDetails
-    {
-        /// <summary>
-        /// Gets or sets the name of the process.
-        /// </summary>
-        public required string ProcessName { get; init; }
-    }
+	/// <summary>
+	/// Represents details of a process, including the name of the process.
+	/// </summary>
+	public readonly struct ProcessDetails
+	{
+		/// <summary>
+		/// Gets or sets the name of the process.
+		/// </summary>
+		public required string ProcessName { get; init; }
+	}
 }
